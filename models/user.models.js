ab36242914a2e2f92303ae85abf8ce77701f6f8f
Om Sonwane <om7sonwane@gmail.com>
2025-03-24 00:58:31 +0530

    user.models.js

--- models/user.models.js
+++ models/user.models.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
     name: String,
-    email: { type: String, unique: true },
+    email: { type: String, required: true, unique: true },
     password: {type: String, required: true},
     role: { type: String, enum: ["admin", "employee"], default: "employee"},
     department: { type: mongoose.Schema.Types.ObjectId, ref: "Department"},
