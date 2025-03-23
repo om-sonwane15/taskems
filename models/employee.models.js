eef99869c602e702492dbd3d710b4d20410f1fe0
Om Sonwane <om7sonwane@gmail.com>
2025-03-24 00:58:09 +0530

    employee.models.js

--- models/employee.models.js
+++ models/employee.models.js
@@ -1,9 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const employeeSchema = new mongoose.Schema({
-    name: { type: String, required: true },
+const UserSchema = new mongoose.Schema({
+    name: String,
     email: { type: String, required: true, unique: true },
-    role: { type: String, required: true },
-    department: { type: String, required: true }
+    password: {type: String, required: true},
+    role: { type: String, enum: ["admin", "employee"], default: "employee"},
+    department: { type: mongoose.Schema.Types.ObjectId, ref: "Department"},
+    profilephoto: {type:String}
 }, { timestamps: true });
-module.exports = mongoose.model('Employee', employeeSchema);
+
+module.exports = mongoose.model("User", UserSchema);
